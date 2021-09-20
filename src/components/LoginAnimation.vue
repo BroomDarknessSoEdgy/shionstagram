150e2840b34f8fb277e0ffbf0cbb08b8b5f311ae
Matthew Tao <goatatia@gmail.com>
2021-09-20 21:05:07 +1200

    fully implemented LoginAnimation

--- src/components/LoginAnimation.vue
+++ src/components/LoginAnimation.vue
@@ -1,30 +1,56 @@
 <template>
-	<div class="login-animation">
-		<div ref="loginForm" class="login-form"></div>
+	<div class="overlay">
+		<div class="login-animation">
+			<div ref="loginBg" class="login-bg"></div>
+			<div ref="loginForm" class="login-form"></div>
+			<div ref="loginTransition" class="login-transition"></div>
+		</div>
 	</div>
 </template>
 
 <script>
 import lottie from "lottie-web";
-import animation from "../assets/login_animation/login_form.json";
+
+import bgAnimation from "../assets/login_animation/login_bg.json";
+import formAnimation from "../assets/login_animation/login_form.json";
+import transitionAnimation from "../assets/login_animation/login_transition.json";
 
 export default {
 	name: "LoginAnimation",
 	mounted() {
 		lottie.loadAnimation({
-			container: this.$refs.loginForm,
-			animationData: animation,
+			container: this.$refs.loginBg,
+			animationData: bgAnimation,
+			loop: false,
 			renderer: "svg",
-			loop: true,
-			autoplay: true,
+			rendererSettings: {
+				preserveAspectRatio: "xMidYMid slice",
+			},
 		});
+		lottie.loadAnimation({
+			container: this.$refs.loginForm,
+			animationData: formAnimation,
+			loop: false,
+		});
+		const transition = lottie.loadAnimation({
+			container: this.$refs.loginTransition,
+			animationData: transitionAnimation,
+			loop: false,
+			renderer: "svg",
+			rendererSettings: {
+				preserveAspectRatio: "xMidYMid slice",
+			},
+		});
+		transition.onComplete = () => {
+			lottie.destroy();
+		};
 	},
 };
 </script>
 
 <style scoped>
-.login-animation {
-	position: absolute;
+.overlay {
+	position: fixed;
 	top: 0;
 	left: 0;
 	right: 0;
@@ -33,8 +59,19 @@ export default {
 	overflow: hidden;
 }
 
-.login-form svg {
-	width: 30rem;
-	height: 30rem;
+.login-animation {
+	position: relative;
+	width: 100%;
+	height: 100%;
+}
+
+.login-bg,
+.login-form,
+.login-transition {
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
 }
 </style>
