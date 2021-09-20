110975418f758ac4b19413abf380166a837b7fe8
Matthew Tao <goatatia@gmail.com>
2021-09-20 12:50:22 +1200

    added box sizing reset

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,35 @@
 <template>
-  <router-view/>
+	<router-view />
 </template>
 
 <style>
+/* Box sizing reset */
+*,
+*::before,
+*::after {
+	box-sizing: border-box;
+	margin: 0;
+	padding: 0;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
 }
 
 #nav {
-  padding: 30px;
+	padding: 30px;
 }
 
 #nav a {
-  font-weight: bold;
-  color: #2c3e50;
+	font-weight: bold;
+	color: #2c3e50;
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+	color: #42b983;
 }
 </style>
