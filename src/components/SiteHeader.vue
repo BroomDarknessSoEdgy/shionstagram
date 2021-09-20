567721c6a608a56f8696f6629d2e20baae73714c
Matthew Tao <goatatia@gmail.com>
2021-09-20 13:46:30 +1200

    implemented site header

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,62 +1,43 @@
 <template>
-    <nav>
-      <div class="header-content">
-        <div class="site-logo">
-            <div>
-              Shionstagram
-            </div>
-          </div>
-        <div class="site-fake-search">
-          <input type="text" placeholder="Search"/>
-        </div>
-        <div class="site-icon-nav">Icons</div>
-      </div>
-    </nav>
+	<header>
+		<nav>
+			<img class="nav-logo" src="../assets/images/Logo.png" alt="" />
+
+			<div class="mock-search">
+				<input type="text" placeholder="Search" />
+			</div>
+			<div class="nav-icons">Icons</div>
+		</nav>
+	</header>
 </template>
 
 <script>
 export default {
-  name: 'SiteHeader',
-  props: {}
-}
+	name: "SiteHeader",
+	props: {},
+};
 </script>
 
 <style scoped>
-div {
-  display: flex;
+header {
+	width: 100%;
+	padding: 1.5rem;
+	background-color: #fff;
 }
 
 nav {
-  height: 54px;
-  width: 100%;
-  z-index: 3;
+	max-width: 1140px;
+	margin: auto;
+	display: grid;
+	grid-template-columns: 1fr 1fr 1fr;
 }
 
-.header-content {
-  display: flex;
-  padding: 0 20px;
-  width: 100%;
-  max-width: 975px;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-  margin: auto;
+.nav-logo {
+	height: 2rem;
+	justify-self: left;
 }
 
-.site-logo {
-  flex-grow: 1;
-}
-
-.site-fake-search {
-  flex-grow: 0;
-  flex-shrink: 1;
-  width: 215px;
-}
-
-.site-icon-nav {
-  flex-grow: 1;
-  justify-content: flex-end;
-  flex: 1 0 0%;
-  flex-wrap: wrap;
+.nav-icons {
+	justify-self: right;
 }
 </style>
