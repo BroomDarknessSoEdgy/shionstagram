fe4ff951391f4ea0d885fba02da7e70eb3c68fd2
Matthew Tao <goatatia@gmail.com>
2021-09-22 11:06:21 +1200

    added postcss-preset-env

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,22 @@
+const postcssPresetEnv = require("postcss-preset-env");
+
 module.exports = {
-    publicPath: '/'
-}
+	publicPath: "/",
+	css: {
+		loaderOptions: {
+			postcss: {
+				plugins: [
+					postcssPresetEnv({
+						autoprefixer: {
+							flexbox: "no-2009",
+						},
+						stage: 3,
+						features: {
+							"custom-properties": false,
+						},
+					}),
+				],
+			},
+		},
+	},
+};
