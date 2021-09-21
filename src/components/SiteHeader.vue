d76a1bc05d0ffa3316e5754036d618aa5ce2fd25
Matthew Tao <goatatia@gmail.com>
2021-09-21 16:41:25 +1200

    added icons to header

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -7,9 +7,21 @@
 		<input class="mock-search" type="text" placeholder="Search" />
 
 		<div class="nav-icons">
-			<router-link to="/">Feed</router-link>
-			<router-link to="/profile">Profile</router-link>
-			<router-link to="/submit">Submit</router-link>
+			<router-link to="/">
+				<img class="icon" src="../assets/icons/Reels.svg" />
+			</router-link>
+			<router-link to="/profile">
+				<img class="icon" src="../assets/icons/Notifiations.svg" />
+			</router-link>
+			<router-link to="/submit">
+				<img class="icon" src="../assets/icons/Send.svg" />
+			</router-link>
+			<router-link to="/profile">
+				<img
+					class="profile-img"
+					src="https://yt3.ggpht.com/AyUL9W0ltc_aJr_MysuZBx8hRfb1SIVNREgU9kiOO-lqmdhYkEsllmhagertVIwHwa3UAAKy=s88-c-k-c0x00ffffff-no-rj"
+				/>
+			</router-link>
 		</div>
 	</header>
 </template>
@@ -26,7 +38,7 @@ header {
 	display: grid;
 	grid-template-columns: 1fr 1fr 1fr;
 	width: 100%;
-	padding: 1rem;
+	padding: 0.5rem 2rem;
 	background-color: #fff;
 	align-items: center;
 }
@@ -48,12 +60,25 @@ header {
 }
 
 .nav-icons {
+	display: flex;
+	justify-content: center;
+	align-items: center;
 	justify-self: right;
 }
-a {
+
+.profile-img {
+	height: 2.5rem;
+	border-radius: 50%;
+	margin-left: 2rem;
+}
+
+.icon {
+	margin-left: 1.5rem;
+}
+
+.nav-icons a {
 	font-weight: bold;
 	color: #444;
 	text-decoration: none;
-	margin-left: 1rem;
 }
 </style>
